195c4121eefc37f22cad3fed71e1dc85c8137c98
Miroslav Thompson <18615545+czmirek@users.noreply.github.com>
2024-02-28 16:33:22 +0100

    commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MauiButtonAlignBug
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowPopup_Clicked(object sender, EventArgs e)
+        {
+            TestPopup testPopup = new TestPopup();
+            Application.Current!.MainPage!.ShowPopup(testPopup);
+        }
+    }
+
+}
---
+++ TestPopup.xaml.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MauiButtonAlignBug;
+
+public partial class TestPopup : Popup
+{   
+    public TestPopup()
+    {
+        InitializeComponent();
+    }
+}
